78d77e0cdfcf4a8ef5d65d11690cf2451a71edfe
JarLob <jarlob@gmail.com>
2025-04-30 12:32:42 +0200

    Allow None sanitizer in cifuzz

--- infra/cifuzz/config_utils.py
+++ infra/cifuzz/config_utils.py
@@ -27,7 +27,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import platform_config
 import constants
 
-SANITIZERS = ['address', 'memory', 'undefined', 'coverage']
+SANITIZERS = ['address', 'memory', 'undefined', 'coverage', 'none']
 
 # TODO(metzman): Set these on config objects so there's one source of truth.
 DEFAULT_ENGINE = 'libfuzzer'
